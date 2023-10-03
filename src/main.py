c595b2a39dde9a04946fad7d64f796010dfb275a
Alexandru Gabriel Lefterache <alex.lefterache@yahoo.com>
2023-10-03 23:53:00 +0300

    Uploaded papers
    I have uploaded some of the most important research papers and also a small piece of code (the replication of the F-CNN architecture).

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import torch
+from torch import nn
+
+import numpy as np
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(torch.cuda.is_available())
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
