e03d0a0f40532e09f413ab6b21487faa890644ce
Alexandru Gabriel Lefterache <alex.lefterache@yahoo.com>
2024-03-18 16:03:22 +0200

    Added LambdaLR with minimum value

--- src/utils/lr_scheduler.py
+++ src/utils/lr_scheduler.py
@@ -9,16 +9,23 @@ def get_lr_scheduler(cfg: dict,
     # Get the scheduler type
     scheduler = cfg['lr_scheduler']
     if scheduler == 'StepLR':
-        # Step scheduler: https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.StepLR.html#torch.optim.lr_scheduler.StepLR
+        # Step scheduler:
+        # https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.StepLR.html#torch.optim.lr_scheduler.StepLR
         return torch.optim.lr_scheduler.StepLR(optimizer=optimizer,
                                                step_size=cfg['lr_step'],
                                                gamma=cfg['lr_gamma'])
     elif scheduler == 'CosineAnnealing':
-        # Cosine annealing schedule: https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.CosineAnnealingWarmRestarts.html#torch.optim.lr_scheduler.CosineAnnealingWarmRestarts
+        # Cosine annealing schedule:
+        # https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.CosineAnnealingWarmRestarts.html#torch.optim.lr_scheduler.CosineAnnealingWarmRestarts
         return torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer=optimizer,
                                                                     T_0=cfg['lr_restart'],
                                                                     T_mult=cfg['lr_mult'],
                                                                     eta_min=cfg['lr_min'])
+    elif scheduler == 'LambdaLR':
+        # LambdaLR:
+        # https://pytorch.org/docs/stable/generated/torch.optim.lr_scheduler.LambdaLR.html
+        return torch.optim.lr_scheduler.LambdaLR(optimizer=optimizer,
+                                                 lr_lambda=lambda epoch: max(0.95 ** epoch, cfg['lr_min'] / cfg['lr']))
     else:
         # Constant learning rate
         return None
